f122ab938bff7ca4a60ae5d970fc4b9b321bfd26
taek <leekt216@gmail.com>
2026-03-13 14:16:46 +0900

    Initial Commit

---
+++ bastion/bastionApp.swift
@@ -0,0 +1,17 @@
+//
+//  bastionApp.swift
+//  bastion
+//
+//  Created by taek on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct bastionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
